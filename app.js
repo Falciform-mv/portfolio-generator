35f85cf5a85188a21e2f5de47262f784c3a18527
Mike Veneziale <falciformdesign@gmail.com>
2021-08-15 19:49:22 -0500

    prints arguments one at a time

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-const profileDataArgs = ProcessingInstruction.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i++) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log('===========');
+
+
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+
+
+printProfileData(profileDataArgs);
